526fa80677f37f7dc4ee351359aaa196953e571a
panda <iospandaman@gmail.com>
2021-01-21 22:56:50 +0900

    Make: initial Project

---
+++ SearchPhotoCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchPhotoCleanSwift
+//
+//  Created by Panda on 2021/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
